2dcf2231d3e84ab477a114b48b753a7fe5515f86
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-07-12 15:57:28 +0600

    controller code done

--- Laravel-Rest-Vue-3/laravel-12-crud/app/Http/Controllers/ProductController.php
+++ Laravel-Rest-Vue-3/laravel-12-crud/app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -28,7 +28,12 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+        ]);
+
+        return Product::create($validated);
     }
 
     /**
@@ -36,7 +41,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return $product;
     }
 
     /**
@@ -52,7 +57,8 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        return $product;
     }
 
     /**
@@ -60,6 +66,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully'], 204);
     }
 }
